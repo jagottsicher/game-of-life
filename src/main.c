fb54ce84db1e2eac774edfc00645bb04cee59164
Jens Schendel <30713344+jagottsicher@users.noreply.github.com>
2020-09-16 17:11:06 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -32,13 +32,16 @@ int width = 30;
 int height = 30;
 int array1[30][30];
 int array2[30][30];
+int i;
+int j;
+int living_cells = 0;
 
 int main()
 {
 // beide Arrays mit 0 füllen.
-for (int i = 1; i < width; i++)
+for (i = 1; i < width; i++)
     {
-        for (int j = 1; j < height; j++)
+        for (j = 1; j < height; j++)
         {
             array1[i][j] = 0;
             array2[i][j] = 0;
@@ -46,19 +49,83 @@ for (int i = 1; i < width; i++)
     }
 
 /*
-Array1 in zwei geschachtelten for Schleifen mit zufällig mit 0 und 1 füllen
 
+// Array1 in zwei geschachtelten for Schleifen mit zufällig mit 0 und 1 füllen
+for (i = 1; i < width; i++)
+    {
+        for (j = 1; j < height; j++)
+        {
+            array1[i][j] = returnRandom(0,1);
+        }
+    }
 
-WHILE
-    Ausgeabefunktion it array1 aufrufen: auf 1/1 setzen und ausgabe durch zwei for schleifen. 0 gibt ein leerzeichen aus, 1 gibt eine *
+// main loop
 
-    array2 in zwei geschachtelten schleifen entlangehen und nach regel befüllen
+while (1)
+{
+//    Ausgeabefunktion it array1 aufrufen: auf 1/1 setzen und ausgabe durch zwei for schleifen. 0 gibt ein leerzeichen aus, 1 gibt eine *
 
-    Ausgeabefunktion mit array2 aufrufen: auf 1/1 setzen und ausgabe durch zwei for schleifen. 0 gibt ein leerzeichen aus, 1 gibt eine *
+// auf Nullpunkt setzen
+printf("\33[%d;%dH", 1, 1);
 
-    array1 in zwei geschachtelten schleifen entlangehen und nach regel befüllen
-DONE
+// printout one becomes *, zero becomes space. Linebreak after each line.
+for (i = 1; i < width; i++)
+    {
+        for (j = 1; j < height; j++)
+        {
+        If (array1[i][j] == 1) 
+            {
+            printf("*");
+            }
+            else
+            {
+            printf(" ");
+            }
+        }
+        printf("\n");    
+    }
 
+//    array2 in zwei geschachtelten schleifen entlangehen und nach regel befüllen
+for (i = 1; i < width; i++)
+    {
+        for (j = 1; j < height; j++)
+        {
+        // anzahl lebender nachbarn auf 0
+        living_cells = 0;
+        // rundum lebende nachbarn zählen
+        i-1,j-1
+        i, j-1
+        i+1, j-1
+        
+        i-1,j
+
+        i+1, j
+        
+        i-1,j+1
+        i, j+1
+        i+1, j+1
+        
+        if () == 1 
+            living_cells++
+        
+        
+        
+        
+        
+        // wenn zelle in array1 an xy lebendig dann
+            wenn anzahl lebender nachbarn < 2 dann zelle xy in array2 auf 0
+            wenn anzahl lebender nachbarn = 2 oder anzahl lebender Zellen = 3 dann zelle xy in array2 auf 1
+            wenn anzahl lebender nachbarn > 3 dann zelle xy in array2 auf 0
+           else
+           if anzahl lebender nachbarn = 3 dann zelle xy in array2 auf 1
+        }
+    }
+
+//    Ausgeabefunktion mit array2 aufrufen: auf 1/1 setzen und ausgabe durch zwei for schleifen. 0 gibt ein leerzeichen aus, 1 gibt eine *
+
+//    array1 in zwei geschachtelten schleifen entlangehen und nach regel befüllen
+
+}
 
     Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
     Any live cell with more than three live neighbours dies, as if by overcrowding.
